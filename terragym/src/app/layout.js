041b7e0af272e884223fd9d6937dd53b51013134
Guars <javierhsilva0@gmail.com>
2025-01-08 10:08:14 -0300

    alo

---
+++ terragym/src/app/layout.js
@@ -0,0 +1,11 @@
+export default function DashboardLayour({ children }) {
+  return ( 
+    <html lang="en">
+      <body>
+      {/* UI DE LAYOUT */}
+      <main>{children}</main>
+
+      </body>
+    </html>
+  )
+}
