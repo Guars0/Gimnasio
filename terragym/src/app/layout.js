25061bb082ffc0170978f7f9680a7c72bf964e8a
Guars <javierhsilva0@gmail.com>
2025-01-12 11:28:47 -0300

    Agregado tailwind

--- terragym/src/app/layout.js
+++ terragym/src/app/layout.js
@@ -1,9 +1,14 @@
-export default function DashboardLayour({ children }) {
+import 'app/globals.css'
+export default function DashboardLayout({ children, }) {
   return ( 
     <html lang="en">
       <body>
       {/* UI DE LAYOUT */}
+      <h1>navbar</h1>
+      <p>contenido</p>
       <main>{children}</main>
+      <p>fin contenido</p>
+      <h1>footer</h1>
 
       </body>
     </html>
---
+++ terragym/src/pages/index.js
@@ -0,0 +1,7 @@
+import 'app/globals.css'
+export default function Page(){
+    return(
+        <h1>aaa</h1>
+    )
+
+}
